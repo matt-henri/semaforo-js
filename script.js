54207b048d9cc9410bd6d020d32ba3e20b7cfe15
Mateus Henrique <mathmarkub@gmail.com>
2022-12-29 13:03:14 -0300

    att

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const imagem = document.getElementById ( 'imagem' )
+const img = document.getElementById ( 'img' )
 const buttons = document.getElementById ( 'buttons' )
 
+const trafficLght = ( event ) => {
+    turnOn[event.target.id]()
+   
+}
 
 
+const turnOn = {
+    'red':    () => img.src = './img/vermelho.png',
+    'yellow': () => img.src = './img/amarelo.png',
+    'green':  () => img.src = './img/verde.png',
+    
+}
+
+
+
+buttons.addEventListener ( 'click', trafficLght) 
